82833665068690b1399be1505a28edbd8e99a34e
henryhoangduong <henryhoangduong@gmail.com>
2025-03-19 22:47:02 +0700

    guards and strategy

---
+++ src/auth/strategies/accessToken.strategy.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(public config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+  async validate(...args: any[]): unknown {}
+}
